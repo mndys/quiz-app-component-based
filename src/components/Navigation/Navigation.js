c7ac54c3c706c9a5ed5e7f2143ee4d074f682cd3
Mandy Schwarze <mandy2301@me.com>
2021-02-08 15:53:40 +0100

    Create Navigation

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,23 @@
+import './Navigation.css'
+import createElement from '../../lib/createElement'
+import homeSVG from '@fortawesome/fontawesome-free/svgs/solid/home.svg'
+import bookmarkSVG from '@fortawesome/fontawesome-free/svgs/solid/bookmark.svg'
+import createSVG from '@fortawesome/fontawesome-free/svgs/solid/plus-circle.svg'
+import settingsSVG from '@fortawesome/fontawesome-free/svgs/solid/sliders.svg'
+
+const buttonsConfig = [
+  { svgPath: homeSVG, text: 'Home' },
+  { svgPath: bookmarkSVG, text: 'Home' },
+  { svgPath: createSVG, text: 'Home' },
+  { svgPath: settingsSVG, text: 'Home' },
+]
+
+export default function Navigation(onNavigate) {
+  buttonsConfig.map(({ svgPath, text }) => {
+    const button = createElement('img', { src: svgPath, alt: text })
+    button.addEventListener('click', () => onNavigate(text))
+    return button
+  })
+  const el = createElement('nav', { className: 'Navigation, ...buttons)' })
+  return el
+}
---
+++ src/components/Navigation/Navigation.stories.js
@@ -0,0 +1,8 @@
+import Navigation from '.'
+
+export default {
+  title: 'Navigation',
+  component: Navigation,
+}
+
+export const base = () => Navigation()
