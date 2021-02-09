d05e1cee8f5d71b9235fdf284cb35a74873a7da6
Mandy Schwarze <mandy2301@me.com>
2021-02-09 15:46:41 +0100

    Change header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,19 @@
 import createElement from '../../lib/createElement'
 import './Header.css'
 
-export default function Header(title, subtitle) {
-  const el = createElement('header', { className: 'Header' })
-
-  const titleEl = createElement('h1', {
-    className: 'Header__title',
-    innerText: title,
-  })
-  const subtitleEl = createElement('span', {
-    className: 'Header__subtitle',
-    innerText: subtitle,
+export default function Header(title) {
+  const el = createElement('header', {
+    className: 'Header',
   })
 
-  el.append(titleEl, subtitleEl)
-  return el
+  setText(title)
+
+  function setText(title) {
+    el.innerHTML = `<h1>${title}</h1>`
+  }
+
+  return {
+    el,
+    setText,
+  }
 }
