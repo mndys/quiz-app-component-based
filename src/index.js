0e5854e37d1188bece5ccabfce4b2ff80cb5420c
Mandy Schwarze <mandy2301@me.com>
2021-02-09 15:47:04 +0100

    Build and improve App-grid

--- src/index.js
+++ src/index.js
@@ -1,15 +1,57 @@
-import Button from './components/Button'
 import Card from './components/Card'
+import CreateForm from './components/CreateForm'
 import Header from './components/Header'
+import Navigation from './components/Navigation/Navigation'
 import createElement from './lib/createElement'
+
+const cards = []
+
+const { el: headerEl } = Header('Quiz App', 'May the best win!')
+
+const navigation = Navigation(onNavigate) // dependency injection
+
+const homePage = createElement('main', { className: 'HomePage', hidden: false })
+
+const form = CreateForm(onSubmit)
+
+const createPage = createElement(
+  'main',
+  { className: 'CreatePage', hidden: true },
+  form
+)
 
 const grid = createElement(
   'div',
   { className: 'appGrid' },
-  Header('Quiz App', 'May the best win!'),
-  Card('Foo bar?', 'Bar baz!'),
-  Button('Click me'),
-  Button('Hold me')
+  headerEl,
+  homePage,
+  createPage,
+  navigation
 )
 
 document.body.append(grid)
+
+function onSubmit(question, answer) {
+  cards.push({ question, answer })
+  renderCards()
+}
+
+function renderCards() {
+  const cardElements = cards.map(({ question, answer }) =>
+    Card(question, answer)
+  )
+  homePage.innerHTML = ''
+  homePage.append(...cardElements)
+}
+
+function onNavigate(text) {
+  if (text === 'Home') {
+    homePage.hidden = false
+    createPage.hidden = true
+  }
+
+  if (text === 'Create') {
+    homePage.hidden = true
+    createPage.hidden = false
+  }
+}
