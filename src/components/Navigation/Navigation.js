ab8f804f00e5e933c112b3245f31b41a206fe16e
Mandy Schwarze <mandy2301@me.com>
2021-02-09 15:46:10 +0100

    Add navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,23 +1,28 @@
-import './Navigation.css'
 import createElement from '../../lib/createElement'
-import homeSVG from '@fortawesome/fontawesome-free/svgs/solid/home.svg'
-import bookmarkSVG from '@fortawesome/fontawesome-free/svgs/solid/bookmark.svg'
-import createSVG from '@fortawesome/fontawesome-free/svgs/solid/plus-circle.svg'
-import settingsSVG from '@fortawesome/fontawesome-free/svgs/solid/sliders-h.svg'
+import homeSvg from '@fortawesome/fontawesome-free/svgs/solid/home.svg'
+import bookmarkSvg from '@fortawesome/fontawesome-free/svgs/solid/bookmark.svg'
+import createSvg from '@fortawesome/fontawesome-free/svgs/solid/edit.svg'
+import settingsSvg from '@fortawesome/fontawesome-free/svgs/solid/cog.svg'
+import './Navigation.css'
 
 const buttonsConfig = [
-  { svgPath: homeSVG, text: 'Home' },
-  { svgPath: bookmarkSVG, text: 'Home' },
-  { svgPath: createSVG, text: 'Home' },
-  { svgPath: settingsSVG, text: 'Home' },
+  { text: 'Home', svgPath: homeSvg },
+  { text: 'Bookmarks', svgPath: bookmarkSvg },
+  { text: 'Create', svgPath: createSvg },
+  { text: 'Settings', svgPath: settingsSvg },
 ]
 
 export default function Navigation(onNavigate) {
-  buttonsConfig.map(({ svgPath, text }) => {
-    const button = createElement('img', { src: svgPath, alt: text })
+  const buttons = buttonsConfig.map(({ svgPath, text }) => {
+    const button = createElement(
+      'button',
+      { className: 'Navigation__button' },
+      createElement('img', { src: svgPath, alt: '' }),
+      text
+    )
     button.addEventListener('click', () => onNavigate(text))
     return button
   })
-  const el = createElement('nav', { className: 'Navigation, ...buttons)' })
-  return el
+
+  return createElement('nav', { className: 'Navigation' }, ...buttons)
 }
